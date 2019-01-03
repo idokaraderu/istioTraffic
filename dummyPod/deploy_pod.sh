c8188801a243e8b47377e4bbc1687d7be68fb5f5
idokaraderu <id.for.googlo.mail@gmail.com>
2019-01-04 06:37:45 +0900

    add deploy script

---
+++ dummyPod/deploy_pod.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#----------------------------------------
+# [What is this?]
+# deploy httpbin pod and service N times
+#----------------------------------------
+
+podLimit=${1:-5}
+namespace=${2:-default}
+manifestPath="./httpbin.yaml"
+sedTarget="httpbin-000"
+
+for i in $(seq $podLimit)
+do
+    paddedNumber=$(printf %03d $i)
+    sed -e "s/$sedTarget/httpbin-$paddedNumber/g" $manifestPath | kubectl $namespace apply -f -
+done
